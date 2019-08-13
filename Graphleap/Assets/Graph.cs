6cd89cf8caf03d2444c10c7242f4ce8bd6de8e2e
KKira <albertovega22@gmail.com>
2019-08-13 12:50:31 -0400

    continuation of the second tutorial

--- Graphleap/Assets/Graph.cs
+++ Graphleap/Assets/Graph.cs
@@ -11,6 +11,9 @@ public class Graph : MonoBehaviour {
 	public int resolution = 10;
 	Transform[] points;
 
+	[Range(0,1)]
+	public int function;
+
 	void Awake () {
 
 		//store in a variable to avoid calculating in the loop everytime as the value is the same
@@ -40,19 +43,48 @@ public class Graph : MonoBehaviour {
 	}
 
 	void Update() {
+		float t = Time.time;
+		//by using delegate, it is possible to invoke f as a function
+		//instead of if else, use an array's index to determine which function
+		static GraphFunction[] functions = {
+			SineFunction, MultiSineFunction
+		};
+		if (function == 0) {
+			f = SineFunction;
+		} else {
+			f = MultiSineFunction;
+		}
+
 		for (int i = 0; i < points.Length; i++) {
 			//get reference of current array element
 			Transform point = points[i];
 			//retrieve that point's position
 			Vector3 position = point.localPosition;
 			//derive y coordinate
+			position.y = f(position.x, t);
+
+			//old way to derive y coordinate
 			//to animate this function add current time to x
 			// we use f(x,t) = sin(pi(x+t)) where t is the elapsed time
-			position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+			//position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
 			//vector are not objects
 			//we adjusted the local variable
 			//to apply to the point we set its position again
 			point.localPosition = position;
 		}
+	}
+
+	//extracting math from the code that loops through the graph points
+	static float SineFunction (float x, float t) {
+		return Mathf.Sin(Mathf.PI * (x + t));
+	}
+
+	static float MultiSineFunction (float x, float t) {
+		float y = Mathf.Sin(Mathf.PI * (x + t));
+		//dpouble the frequency, and half it to keep the shape of the sine wave
+		y += Mathf.Sin(2f * Mathf.PI * (x + 2f * t)) / 2f;
+		//to guarantee that the values stay within -1-1 range we multiply by 2/3
+		y *= 2f / 3f;
+		return y;
 	}
 }
